13f981f7e711d7b22f73a263f91863bb8faafe79
TPWeb <info@tpweb.org>
2020-10-01 23:52:09 +0200

    Update validateTest.php

--- tests/validateTest.php
+++ tests/validateTest.php
@@ -64,6 +64,8 @@ class validateTest extends TestCase
         $translator->shouldReceive('trans')->once()->with('validation.attributes.foo')->andReturn('validation.attributes.foo');
         $translator->shouldReceive('get')->once()->with('validation.custom.foo.iban')->andReturn('validation.custom.foo.iban');
         $translator->shouldReceive('get')->once()->with('validation.custom')->andReturn('validation.custom');
+        $translator->shouldReceive('get')->once()->with('validation.iban')->andReturn('validation.iban');
+        $translator->shouldReceive('get')->once()->with('validation.attributes.foo')->andReturn('validation.attributes.foo');
 
         $factory = new Factory($translator, $container);
         $factory->extend('iban', 'TPWeb\Iban\Validation\ValidatorExtensions@validateIban', ':attribute must be a valid IBAN number');
@@ -109,6 +111,8 @@ class validateTest extends TestCase
         $translator->shouldReceive('trans')->once()->with('validation.attributes.foo')->andReturn('validation.attributes.foo');
         $translator->shouldReceive('get')->once()->with('validation.custom.foo.bic')->andReturn('validation.custom.foo.bic');
         $translator->shouldReceive('get')->once()->with('validation.custom')->andReturn('validation.custom');
+        $translator->shouldReceive('get')->once()->with('validation.bic')->andReturn('validation.bic');
+        $translator->shouldReceive('get')->once()->with('validation.attributes.foo')->andReturn('validation.attributes.foo');
 
         $factory = new Factory($translator, $container);
         $factory->extend('bic', 'TPWeb\Iban\Validation\ValidatorExtensions@validateBic', ':attribute must be a valid IBAN number');
